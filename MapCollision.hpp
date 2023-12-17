bef3d88de931d0a8c1e5732effe5ede78f3595f7
TinKendel <116545230+TinKendel@users.noreply.github.com>
2023-12-17 11:14:15 +0100

    Delete MapCollision.hpp

--- MapCollision.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Sounds.hpp"
-
-
-bool map_collision(bool i_collect_pellets, bool i_use_door, short i_x, short i_y, std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& i_map, SoundManager& gameSounds);
-
-//bool scoreValue(short i_x, short i_y, std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& i_map);
